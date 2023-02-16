2dd5fb094b6c587116b659417ae05da1cb54e0ab
jverdu-r <102660580+jverdu-r@users.noreply.github.com>
2023-02-16 18:41:24 +0100

    Add files via upload

---
+++ inc/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 16:09:50 by jverdu-r          #+#    #+#             */
+/*   Updated: 2023/02/16 18:17:07 by jverdu-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PIPEX_H
+# define PIPEX_H
+
+# include "./libft/libft.h"
+# include "./printf/printf.h"
+# include <sys/wait.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+
+
+
+#endif
