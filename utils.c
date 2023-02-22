fc51ede18e71917806b46d1c70a840a78f07d8c5
jverdu-r <102660580+jverdu-r@users.noreply.github.com>
2023-02-22 19:41:32 +0100

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 18:34:58 by jverdu-r          #+#    #+#             */
+/*   Updated: 2023/02/22 18:06:07 by jverdu-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/pipex.h"
+
+char	*pathfinder(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(path, cmd);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = 0;
+	while (paths[i++])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+void	error(void)
+{
+	perror("\033[31mError");
+	exit(EXIT_FAILURE);
+}
+
+void	executor(char *argv, char **envp)
+{
+	char	**cmd;
+	char	*path;
+	int		i;
+
+	i = 0;
+	cmd = ft_split(((const char *)argv), ' ');
+	path = pathfinder(cmd[0], envp);
+	if (!path)
+	{
+		while (cmd[i++])
+			free(cmd[i]);
+		free(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
+}
