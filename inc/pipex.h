737faa0d4f4a39740aa2c9230f16d30a707874c8
jverdu-r <102660580+jverdu-r@users.noreply.github.com>
2023-03-02 17:29:31 +0100

    Add files via upload

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,11 +6,11 @@
 /*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 16:09:50 by jverdu-r          #+#    #+#             */
-/*   Updated: 2023/02/16 18:17:07 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:39:38 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	PIPEX_H
+#ifndef PIPEX_H
 # define PIPEX_H
 
 # include "./libft/libft.h"
@@ -19,12 +19,12 @@
 # include <fcntl.h>
 # include <errno.h>
 
-int	    main(int argc, char **argv, char **envp);
-int     open_file(char *argv, int i);
-int	    gnllite(char **line);
-void    executor(char *argv, char **envp);
-void    error();
-void    usage(void);
-
+int		main(int argc, char **argv, char **envp);
+int		open_file(char *argv, int i);
+int		gnllite(char **line);
+char	*pathfinder(char *cmd, char **envp);
+void	executor(char *argv, char **envp);
+void	error(void);
+void	usage(void);
 
 #endif
